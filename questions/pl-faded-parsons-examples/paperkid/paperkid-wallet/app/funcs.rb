6a2dc0ffac8872b5809a3adf95a8d3ffe766d1a6
mverdicchio <verdicchio@gmail.com>
2022-09-14 15:49:18 -0400

    upgrades to Wallet for micropilot (still in progress)

--- questions/pl-faded-parsons-examples/paperkid/paperkid-wallet/app/funcs.rb
+++ questions/pl-faded-parsons-examples/paperkid/paperkid-wallet/app/funcs.rb
@@ -1,19 +1,21 @@
-class InsufficientFundsError < StandardError
-end
-
 class Wallet
     attr_reader :cash
+    attr_reader :error
     def initialize(amount)
       raise ArgumentError if amount < 0
       @cash = amount
     end
     def withdraw(amount)
-      raise InsufficientFundsError if amount > @cash
-      raise ArgumentError if amount < 0
+      if amount > @cash or amount < 0 
+        @error = 'invalid request'
+        return nil
+      end
       @cash -= amount
+      @error = nil
       return amount
     end
   end
 
 
+  #todo: add rdocs
   
