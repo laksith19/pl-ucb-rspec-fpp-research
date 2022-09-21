999c70016208f0161ef45d25955101248ece1cab
mverdicchio <verdicchio@gmail.com>
2022-09-21 15:42:13 -0400

    added rdocs for Wallet

--- questions/pl-faded-parsons-examples/paperkid/paperkid-wallet/app/funcs.rb
+++ questions/pl-faded-parsons-examples/paperkid/paperkid-wallet/app/funcs.rb
@@ -1,10 +1,33 @@
+# Wallet
+#
+# Maintains an amount of cash and an error
+# string.
 class Wallet
+
+    # The amount of money in the wallet
     attr_reader :cash
+
+    # An error string, nil if no errors
     attr_reader :error
-    def initialize(amount)
+
+    # Constructor
+    #
+    # @raise [ArgumentError] if amount < 0
+    #
+    # @param [amount] amount to set in the wallet
+    def initialize(amount) #:notnew:
       raise ArgumentError if amount < 0
       @cash = amount
     end
+    
+    # Withdraw
+    #
+    # Sets Wallet#error string after invalid request.
+    # It sets Wallet#error string to +nil+ after valid requests
+    #
+    # @param [amount] amount to withdraw
+    #
+    # @return [truthy] for valid withdraws
     def withdraw(amount)
       if amount > @cash or amount < 0 
         @error = 'invalid request'
