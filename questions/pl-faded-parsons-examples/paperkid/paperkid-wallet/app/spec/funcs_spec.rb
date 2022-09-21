dd9104c22da8ce53268142f1991c78ec3d02eca5
mverdicchio <verdicchio@gmail.com>
2022-09-21 15:16:17 -0400

    debugged wallet withdraw tests

--- questions/pl-faded-parsons-examples/paperkid/paperkid-wallet/app/spec/funcs_spec.rb
+++ questions/pl-faded-parsons-examples/paperkid/paperkid-wallet/app/spec/funcs_spec.rb
@@ -11,40 +11,26 @@ describe 'Wallet Tests' do
         end
     end
 
-    # these tests still need to be checked.
     describe 'withdraw' do
         before(:each) do
-            amount = 20
-            amount_to_remove = 10
-            wallet = Wallet.new(amount)
+            @amount = 20
+            @amount_to_remove = 10
+            @wallet = Wallet.new(@amount)
         end
         it 'should remove valid amount' do
-            wallet.withdraw(amount_to_remove)
-            expect( wallet.cash ).to eq(amount - amount_to_remove)
-            expect( wallet.error ).to be_falsy
+            @wallet.withdraw(@amount_to_remove)
+            expect( @wallet.cash ).to eq(@amount - @amount_to_remove)
+            expect( @wallet.error ).to be_falsy
         end
         it 'should not withdraw too much' do
-            wallet.withdraw(amount + 1)
-            expect( wallet.error ).to be_truthy
-            expect( wallet.cash ).to eq(amount)
-            # not_to change? check valid rspec matchers...
+            @wallet.withdraw(@amount + 1)
+            expect( @wallet.error ).to be_truthy
+            expect( @wallet.cash ).to eq(@amount)
         end
         it 'should not withdraw negative money' do
-            wallet.withdraw(-1 * amount)
-            expect( wallet.error ).to be_truthy
-            expect( wallet.cash ).to eq(amount)
-        end
-    end
-    # extra tests...
-    describe 'getter setter' do
-        it 'should have cash set by constructor' do
-            amount = 20
-            wallet = Wallet.new(amount)
-            expect(wallet.cash).to eq(amount)
-        end
-        it 'should not have a cash setter method' do
-            wallet = Wallet.new(10)
-            expect{ wallet.cash = 100 }.to raise_error(NoMethodError)
+            @wallet.withdraw(-1 * @amount)
+            expect( @wallet.error ).to be_truthy
+            expect( @wallet.cash ).to eq(@amount)
         end
     end
 end
